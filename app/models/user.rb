488c8b776fa1c5b210ca7459eaf1f988005aa16f
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-17 17:23:44 +0900

    単体テストコード完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,16 @@ class User < ApplicationRecord
          validates :last_name_kana, presence: true
          validates :first_name_kana, presence: true
          validates :birthday_date, presence: true
+         
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX
 
+        with_options presence: true do
+
+          validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input Kanji characters."}
+          validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input Kanji characters."}
+          validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+          validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+
+ end
 end
