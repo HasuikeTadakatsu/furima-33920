ffffca7d9510891a8969e9ad16fe75b4ce36ba3c
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-17 21:03:06 +0900

    ユーザ管理機能(修正2回目)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,12 @@ class User < ApplicationRecord
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX
 
-         with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input Kanji characters."} true do
+         with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input Kanji characters."} do
          validates :last_name
          validates :first_name
          end
 
-         with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} true do
+         with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
          validates :last_name_kana
          validates :first_name_kana
          end
