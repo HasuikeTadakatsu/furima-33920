192c97bf784f30b7ec93572970668f3135aae4a2
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-19 16:32:12 +0900

    editアクションを定義する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,13 @@ class ItemsController < ApplicationController
       end
     end
 
+    def edit
+      @item = Item.find(params[:id])
+      unless user_signed_in? && current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    end
+
   private
 
   def item_params
