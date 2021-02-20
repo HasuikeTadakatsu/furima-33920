790581419c53381c1a1558f57c1bc34b1baeda69
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-20 14:45:13 +0900

    バリデーションの処理を書く

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,4 +1,29 @@
 class ItemOrder
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+  validates :token 
+  validates :postal_code
+  validates :prefecture_id
+  validates :municipality
+  validates :address
+  validates :building_name
+  validates :phone_number
+  end
+
+  with_options presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ } do
+  validates :postal_code, length: { is: 8 } 
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
+  validates :prefecture_id
+  end
+
+  with_options presence: true, format: { with: /\A\d{11}\z/ } do
+  validates :phone_number
+  end
+
+
+
 end
