7bbe7a383c2e4c4ec5dce4b21a2be8cdb848204b
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-03-01 21:04:06 +0900

    チャットページ作成(見た目反映)

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,11 @@
 class ChatsController < ApplicationController
 
   def index
+    @chats = Chat.all.order("created_at DESC")
+  end
+
+  def create
+    Chat.create(chat: params[:chat])
   end
 
 end
